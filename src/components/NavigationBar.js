032c73595d4da9c0684b646f0644169e20a90cd1
G00394925 <G00394925@atu.ie>
2024-11-30 17:41:46 +0000

    Create Navbar and homepage

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,18 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavigationBar = () => {
+  return (
+        <Navbar bg="dark" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand href="/">Home</Navbar.Brand>
+            <Nav className="me-auto">
+            </Nav>
+          </Container>
+      </Navbar>
+  );
+};
+
+export default NavigationBar;
