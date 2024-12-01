2203c7bc14f19ae97a2ae944dfcff31455f51df1
G00394925 <G00394925@atu.ie>
2024-12-01 16:53:11 +0000

    Cards display side by side

--- src/components/music.js
+++ src/components/music.js
@@ -1,9 +1,14 @@
 import MusicItem from './musicitem';
 
-function Music(props) {
-    return props.mymusic.map((music) => (
-      <MusicItem mymusic={music} key={null} />
-    ));
-  }
+const Music = (props) => {
+    return (
+        // Each music item has its own container with the card-container class - Allows the cards to be displayed side by side
+        <div className="card-container"> 
+            {props.mymusic.map((music) => (
+              <MusicItem mymusic={music} key={null} />
+            ))}
+        </div>
+  )
+}
 
 export default Music;
--- src/components/musicitem.js
+++ src/components/musicitem.js
@@ -8,17 +8,15 @@ const MusicItem = (props) => {
     }, [props.mymusic])
  
     return(
-        <div>
-            <Card className="bg-secondary" style={{ width: '16rem', margin: '10px'}}>
-                <img src={props.mymusic.Cover} alt={props.mymusic.Title}></img>
-                <Card.Title style={{ marginLeft: '5px'}}>{props.mymusic.Title}</Card.Title>
-                <Card.Subtitle style={{ marginLeft: '5px'}}>
-                    {props.mymusic.Artist} 
-                    <br/>
-                    {props.mymusic.Year}
-                </Card.Subtitle>
-            </Card>
-        </div>
+        <Card className="bg-secondary" style={{ color: '#c7c7c7', width: '16rem', margin: '10px'}}>
+            <img src={props.mymusic.Cover} alt={props.mymusic.Title}></img>
+            <Card.Title style={{ marginLeft: '5px'}}>{props.mymusic.Title}</Card.Title>
+            <Card.Subtitle style={{ marginLeft: '5px'}}>
+                {props.mymusic.Artist} 
+                <br/>
+                {props.mymusic.Year}
+            </Card.Subtitle>
+        </Card>
     )
 }
 
