876c5c547598e920fbc0bc705c7c3d5b6d3b98fc
G00394925 <G00394925@atu.ie>
2024-12-02 13:29:50 +0000

    TODO: Need to fix client not retrieving data from server

--- src/components/view.js
+++ src/components/view.js
@@ -1,5 +1,6 @@
 import {useEffect , useState} from "react";
 import Music from './music';
+import axios from "axios";
 
 const data = [
     {
@@ -23,10 +24,23 @@ const data = [
 ]
 
 const View = () => {
+
+    const [music, setMusic] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:4000/api/music')
+            .then((response) => {
+                console.log(response.data)
+                setMusic(response.data.music);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    })
     return(
         <div>
             <h1>This is where the music happens</h1>
-            <Music mymusic={data} />
+            <Music mymusic={music} />
         </div>
     );
 }
